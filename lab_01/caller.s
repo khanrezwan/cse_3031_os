d26eb4057794a4321d03e0b0f894cd09766ad47f
khanrezwan <rezwanislam@gmail.com>
2016-05-25 01:57:15 +0600

    updated caller.s

--- lab_01/caller.s
+++ lab_01/caller.s
@@ -9,11 +9,17 @@ global _start
   
   section .text
     _start:
+    
     push arg3
   push  arg2
     push arg1
-    
+    ; call subroutine
     call sum_of_three
+    ; remove arguments from frame
+    add     esp, 12 ; remove 3 words  or 3 * 4 Bytes or 3 * 32/8 Bytes
+	
+	; restore old call frame
+	pop     ebp
     mov eax,1            ; The system call for exit (sys_exit)
 	mov ebx,0            ; Exit with return code of 0 (no error)
 	int 80h
